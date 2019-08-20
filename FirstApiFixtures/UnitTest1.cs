8b16c182387c38355035e0732af8817f19dae3a1
ahari@tavisca.in <ahari@tavisca.in>
2019-08-20 14:02:44 +0530

    initial

---
+++ FirstApiFixtures/UnitTest1.cs
@@ -0,0 +1,28 @@
+using FirstApi.Controllers;
+using System;
+using Xunit;
+
+namespace FirstApiFixtures
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestHelloReply()
+        {
+            ValuesController greet = new ValuesController();
+            string actual = greet.Get("Hello").Value;
+            string expected = "Hii";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestHiiReply()
+        {
+            ValuesController greet = new ValuesController();
+            string actual = greet.Get("Hii").Value;
+            string expected = "Hello";
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
